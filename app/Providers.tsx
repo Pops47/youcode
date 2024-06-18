f7e67890de1d82bf129a53b84fae5f07e056d039
Pauline Soubrie <pauline_soubrie@hotmail.fr>
2024-06-18 16:34:14 +0200

    T1 - config

---
+++ app/Providers.tsx
@@ -0,0 +1,25 @@
+// app/Providers.
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren } from "react";
+import { Toaster } from "sonner";
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Toaster />
+        <SessionProvider>
+          <QueryClientProvider client={queryClient}>
+            {children}{" "}
+          </QueryClientProvider>
+        </SessionProvider>
+      </ThemeProvider>
+    </>
+  );
+};
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,32 @@
+// src/components/layout/Header.
+import { SiteConfig } from "@/lib/site-config";
+import Image from "next/image";
+import Link from "next/link";
+import { ThemeToggle } from "../ThemeToggle";
+import { Typography } from "../Typography";
+
+export function Header() {
+  return (
+    <header className="bg-background sticky top-0 z-40 w-full border-b">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <div className="flex gap-4 md:gap-4 items-center">
+          <Image
+            src="/images/logo.svg"
+            width={50}
+            height={35}
+            alt="youcode logo"
+          />
+          <Typography variant="h3" as={Link} href="/">
+            {SiteConfig.title}
+          </Typography>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
