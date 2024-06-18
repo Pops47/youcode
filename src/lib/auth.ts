b06577af737ec0caab198896322583b9eaf48949
Pauline Soubrie <pauline_soubrie@hotmail.fr>
2024-06-19 00:13:19 +0200

    change path for app router

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,7 @@ import {
   NextApiResponse,
 } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import { authOptions } from "../../app/api/auth/[...nextauth]/route";
 
 type ParametersGetServerSession =
   | []
